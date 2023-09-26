a00ceaacf82aff289728a6d48e0fa81f131053f1
Shadid516 <shadid516@gmail.com>
2023-09-26 23:51:19 +0300

    working on SquaresofaSortedArray

--- src/apps/App.java
+++ src/apps/App.java
@@ -1,12 +1,10 @@
 package apps;
 
-import beginner.MergeSortedArray;
+import beginner.*;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-
-        MergeSortedArray.merge2ElectricBoogaloo(new int[] { 1, 2, 3, 6, 0, 0 }, 4, new int[] { 2, 7 }, 2);
         MergeSortedArray.merge2ElectricBoogaloo(new int[] { 1, 2, 3, 6, 0, 0 }, 4, new int[] { 2, 7 }, 2);
     }
 
--- src/beginner/SquaresofaSortedArray.java
+++ src/beginner/SquaresofaSortedArray.java
@@ -1,5 +1,36 @@
 package beginner;
 
-public class SquaresofaSortedArray {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
+public class SquaresofaSortedArray {
+    public int[] sortedSquares(int[] nums) {
+        int nlength = nums.length;
+        int smallest;
+        int[] output = new int[nlength];
+        for (int i : nums) {
+            i = i * i;// squaring all numbers
+        }
+        if (nums[0] < nums[nlength - 1]) {
+            // in order to find smallest value we need to start from biggest
+            // since we squared a sorted list the highest value will be either in the
+            // beginning or end of the array
+            smallest = nums[nlength - 1];
+        } else {// if numbers on both ends of array are equal or if num at [0] is bigger,
+                // nums[0] will be the value of smallest
+            smallest = nums[0];
+        }
+        for (int i : nums) {// iterating to find smallest variable
+            if (i < smallest) {
+                smallest = i;
+            }
+        }
+        // condition checks if nums length is odd or even
+        if (nlength % 2.0 == 0) {
+            int i1 = nlength / 2;
+            int i2 = ++i1;
+        }
+
+        return output;
+    }
 }
